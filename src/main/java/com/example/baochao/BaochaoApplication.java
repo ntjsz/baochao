0276e6704f6041d945e5a125c060058ef658e8f4
Haitao Hu <lakehaitao@gmail.com>
2020-12-18 18:34:43 +0800

    init

---
+++ src/main/java/com/example/baochao/BaochaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.baochao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaochaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaochaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/baochao/BaochaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baochao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaochaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
