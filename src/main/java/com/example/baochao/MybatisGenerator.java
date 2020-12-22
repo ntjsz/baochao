99edc7111e68e2281c64bc7e90c89390e7bb37e6
Haitao Hu <lakehaitao@gmail.com>
2020-12-22 15:51:59 +0800

    run mybatis generator with java

---
+++ src/main/java/com/example/baochao/MybatisGenerator.java
@@ -0,0 +1,29 @@
+package com.example.baochao;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MybatisGenerator {
+    public static void main(String[] args) {
+        List<String> warnings = new ArrayList<>();
+        try {
+            File configFile = new File("generatorConfig.xml");
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(configFile);
+            DefaultShellCallback callback = new DefaultShellCallback(true);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("========= warnings =========");
+            System.out.println(warnings);
+        }
+    }
+}
