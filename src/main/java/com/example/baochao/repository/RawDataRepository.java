d72179d620bba2d8b2ada0a8d1ce2c537b09737e
Haitao Hu <lakehaitao@gmail.com>
2020-12-25 22:33:10 +0800

    食材

--- src/main/java/com/example/baochao/repository/RawDataRepository.java
+++ src/main/java/com/example/baochao/repository/RawDataRepository.java
@@ -1,11 +1,17 @@
 package com.example.baochao.repository;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @Repository
 public class RawDataRepository {
@@ -16,10 +22,15 @@ public class RawDataRepository {
     @Value("${baochao.data.file.location}")
     private String dataFileLocation;
 
-    @Cacheable(cacheNames = "rawData", sync = true)
-    public String getRawData() {
+    //@Cacheable(cacheNames = "rawData", sync = true)
+    public JsonNode getRawData() {
         Resource resource = applicationContext.getResource("classpath:" + dataFileLocation);
-        System.out.println(dataFileLocation);
-        return resource.getFilename();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readTree(resource.getFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- src/main/java/com/example/baochao/service/DatabaseService.java
+++ src/main/java/com/example/baochao/service/DatabaseService.java
@@ -1,6 +1,8 @@
 package com.example.baochao.service;
 
+import com.example.baochao.domain.common.GameInfo;
 import com.example.baochao.repository.RawDataRepository;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
@@ -14,7 +16,12 @@ public class DatabaseService {
     private RawDataRepository rawDataRepository;
 
 
-    public void refresh() {
-        rawDataRepository.getRawData();
+    public GameInfo refresh() {
+        JsonNode documentNode = rawDataRepository.getRawData();
+
+        GameInfo gameInfo = new GameInfo();
+        gameInfo.parse(documentNode);
+
+        return gameInfo;
     }
 }
